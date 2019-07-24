7445b80772124caee15e9e7818db5e07751bba6b
Abdul <abdul.alim.uk@gmail.com>
2019-07-24 13:55:45 +0100

    your commit comment

---
+++ cashWithdrawal.js
@@ -0,0 +1,7 @@
+let accnumber = 50449921;
+const cashWithdrawal = (amount, accnum) => {
+ console.log(`Withdrawing ${amount} from account ${accnum}`);
+}
+cashWithdrawal(300, accnumber);
+cashWithdrawal(30, 50449921);
+cashWithdrawal(200, 50447921); 
